6894234cc9d9ee7df98628c6b2ead1684dded025
RebeccaPedler <rebecca.pedler@yumbah.com>
2025-11-07 13:42:51 +1030

    Add files via upload

---
+++ Code/Deduplicate.R
@@ -0,0 +1,52 @@
+# Load required libraries
+library(tidyverse)
+library(janitor)
+library(stringr)
+
+install.packages(c("stringr","janitor", "janitor"))
+
+# Set working directory
+setwd("C:/Users/RebeccaPedler/OneDrive - Yumbah/Documents/R&D/Industry PhD/Trials/Meta analysis/SEARCHES")
+
+# Define the file name
+df <- read.csv("WOS and SCOPUS 11092025.csv", header = TRUE, stringsAsFactors = FALSE)
+summary(df)
+
+# Step 1: Count original rows
+original_n <- nrow(df)
+print(original_n)
+
+# Step 2: Clean and remove duplicates
+df_unique <- df %>%
+  mutate(
+    Title_clean = Title %>%
+      str_to_lower() %>%                     
+      str_replace_all("[[:punct:]]", "") %>% 
+      str_replace_all("\\s+", " ") %>%       
+      str_trim()                             
+  ) %>%
+  distinct(Title_clean, .keep_all = TRUE) %>%
+  select(-Title_clean)
+
+# Step 3: Count cleaned rows
+cleaned_n <- nrow(df_unique)
+
+# Step 4: Report how many duplicates were removed
+cat("Original rows:", original_n, "\n")
+cat("Rows after removing duplicates:", cleaned_n, "\n")
+cat("Duplicates removed:", original_n - cleaned_n, "\n")
+
+# Step 5: Write to CSV
+# Set working directory
+setwd("C:/Users/RebeccaPedler/OneDrive - Yumbah")
+write.csv(df_unique, "cleaned_references.csv", row.names = FALSE)
+
+> cat("Original rows:", original_n, "\n")
+Original rows: 3068 
+> cat("Rows after removing duplicates:", cleaned_n, "\n")
+Rows after removing duplicates: 2132 
+> cat("Duplicates removed:", original_n - cleaned_n, "\n")
+Duplicates removed: 936 
+> # Step 5: Write to CSV
+> write.csv(df_unique, "cleaned_references.csv", row.names = FALSE)
+>
---
+++ Code/MetaDigitiser.R
@@ -0,0 +1,335 @@
+# Set the path to your image directory (folder containing your JPEG)
+image_dir <- "C:/Users/RebeccaPedler/OneDrive - Yumbah/Documents/R&D/Industry PhD/Trials/Meta analysis/Ulva benchmark papers/Falade_graphics/Mean_and_error"
+
+library(metaDigitise)
+
+# Start the digitising interface
+digitised_data <- metaDigitise(image_dir, summary = TRUE)
+print(digitised_data)
+
+#Image proccessing
+#S006.1_CF
+1
+d
+c
+m
+final condition factor
+0.98
+1.14
+n
+y
+control
+5
+a
+IMTA
+5
+a
+non-IMTA
+5
+f
+CI95
+n
+
+#S006.1_FinalWeight
+1
+d
+c
+m
+Final weight (g)
+100
+122
+n
+y
+control
+5
+a
+IMTA
+5
+a
+non-IMTA
+5
+f
+CI95
+n
+
+#S006.1_SGR
+1
+d
+c
+m
+SGR
+0.17
+0.26
+n
+y
+control
+5
+a
+IMTA
+5
+a
+non-IMTA
+5
+f
+CI95
+n
+
+#S006.2_FCR
+y
+c
+m
+FCR
+1.0
+4.5
+n
+y
+IMTA-U0
+3
+a
+IMTA-U0.75
+3
+a
+IMTA-U1.5
+3
+a
+IMTA-U3
+3
+a
+IMTA-U6
+3
+a
+IMTA-U12
+3
+f
+CI95
+y
+
+#S006.2_FEED INTAKE
+c
+m
+FEED INTAKE
+5.0
+11.0
+n
+y
+IMTA-U0
+3
+a
+IMTA-U0.75
+3
+a
+IMTA-U1.5
+3
+a
+IMTA-U3
+3
+a
+IMTA-U6
+3
+a
+IMTA-U12
+3
+f
+CI95
+y
+
+#S006.2_PER
+c
+m
+PER
+1.0
+3.0
+n
+y
+IMTA-U0
+3
+a
+IMTA-U0.75
+3
+a
+IMTA-U1.5
+3
+a
+IMTA-U3
+3
+a
+IMTA-U6
+3
+a
+IMTA-U12
+3
+f
+CI95
+y
+
+# Set the path to your image directory (folder containing your JPEG)
+image_dir <- "C:/Users/RebeccaPedler/OneDrive - Yumbah/Documents/R&D/Industry PhD/Trials/Meta analysis/Ulva benchmark papers/Mwangudza_graphics"
+
+# Start the digitising interface
+digitised_data_Mwu <- metaDigitise(image_dir, summary = TRUE)
+print(digitised_data_Mwu)
+
+#Final length
+1
+d
+c
+m
+length(mm)
+54
+70
+n
+y
+Abfeed (Reference) 30 days
+4
+a
+Abfeed (Reference) 120 days
+4
+a
+Abfeed (Reference) 150 days
+4
+a
+Biosecure Ulva 30 days
+4
+a
+Biosecure Ulva 120 days
+4
+a
+Biosecure Ulva 150 days
+4
+a
+Not biosecure Ulva 30 days
+4
+a
+Not biosecure Ulva 120 days
+4
+a
+Not biosecure Ulva 150 days
+4
+f
+se
+n
+
+#Final weight
+1
+d
+c
+m
+weight(g)
+0
+65
+n
+y
+Abfeed (Reference) 30 days
+4
+a
+Abfeed (Reference) 120 days
+4
+a
+Abfeed (Reference) 150 days
+4
+a
+Biosecure Ulva 30 days
+4
+a
+Biosecure Ulva 120 days
+4
+a
+Biosecure Ulva 150 days
+4
+a
+Not biosecure Ulva 30 days
+4
+a
+Not biosecure Ulva 120 days
+4
+a
+Not biosecure Ulva 150 days
+4
+f
+se
+n
+
+y)
+
+# Set the path to your image directory (folder containing your JPEG)
+image_dir <- "C:/Users/RebeccaPedler/OneDrive - Yumbah/Scatter plots"
+
+install.packages("metaDigitise")
+library(metaDigitise)
+
+# Start the digitising interface
+digitised_data <- metaDigitise(image_dir, summary = TRUE)
+print(digitised_data)
+
+#Image proccessing
+#S006.2_CF
+1
+s
+s
+c
+CF
+Ulva inclusion
+0
+1.6
+0
+15
+n
+n
+IMTA-U0
+IMTA-U0.75
+IMTA-U1.5
+IMTA-U3
+IMTA-U6
+IMTA-U12
+c
+a
+f
+n
+y
+
+#Image proccessing
+#S006.2_FINAL SHELL LENGTH
+c
+FSL
+Ulva inclusion
+68
+76
+0
+15
+n
+n
+IMTA-U0
+IMTA-U0.75
+IMTA-U1.5
+IMTA-U3
+IMTA-U6
+IMTA-U12
+c
+a
+f
+n
+y
+
+#Image proccessing
+#S006.2_SGR
+c
+SGR
+Ulva inclusion
+0
+0.40
+0
+15
+n
+n
+IMTA-U0
+IMTA-U0.75
+IMTA-U1.5
+IMTA-U3
+IMTA-U6
+IMTA-U12
+c
+a
+f
+n
+y
+
