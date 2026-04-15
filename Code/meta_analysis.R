b1bb4ef5ce2af61b24c49bbe59506f8c80da641c
RebeccaPedler <rebecca.pedler@yumbah.com>
2026-04-15 10:58:13 +0930

    Update meta_analysis.R

--- Code/meta_analysis.R
+++ Code/meta_analysis.R
@@ -29,7 +29,7 @@ for (p in packages) {
 
 ###Please download GitHub repository and then run the following
 here()
-clean_data <- read_csv(here("Data", "cleaned_data_for_meta_analysis.csv"))
+clean_data <- read_csv("cleaned_data_for_meta_analysis.csv")
 head(clean_data)
 
 # Calculate effect size (lnRR) and variance
@@ -96,11 +96,6 @@ VCV <- vcalc(
   data = clean_data
 )
 
-# Check VCV
-dim(res_3L_all$V)           # should be n x n
-is.matrix(res_3L_all$V)     # should return TRUE
-str(res_3L_all$V)
-
 ## Run meta-analysis
 # All clean_data (overall model)
 res_3L_all <- rma.mv(yi = lnRR, V = VCV,
@@ -109,6 +104,10 @@ res_3L_all <- rma.mv(yi = lnRR, V = VCV,
                      method = "REML")
 res_3L_all
 
+# Check VCV
+dim(res_3L_all$V)           # should be n x n
+is.matrix(res_3L_all$V)     # should return TRUE
+str(res_3L_all$V)
 
 ## Create function for extracting variance components
 calc_I2_3level <- function(model) {
@@ -202,9 +201,163 @@ summary(res_species_random)
 # Compare models
 anova(res_3L_all, res_species_random)
 
-#### Adding species as a random effect does not improve model fit, continue without.
+#### Adding species as a random effect does not improve model fit, continue without
 
-## Testing if broadoutcome category is a significant moderator
+## Publication bias
+
+#Function for publication bias models and plots
+run_bias_models <- function(data, 
+                             yi = "lnRR",
+                             vi = "vi_lnRR",
+                             vcv = NULL,
+                             study_id = "study_ID",
+                             es_id = "ES_ID",
+                             n_treatment = "treatment_n",
+                             n_control = "control_n",
+                             year_var = "publication_year",
+                             bio_moderators = NULL) {
+  
+  # Compute sqrt_inv_e_n
+  data$sqrt_inv_e_n <- sqrt(
+    (data[[n_treatment]] + data[[n_control]]) /
+    (data[[n_treatment]] * data[[n_control]])
+  )
+  
+  # Build random effects formula
+  random_fx <- list(
+    as.formula(paste0("~1 | ", study_id)),
+    as.formula(paste0("~1 | ", es_id))
+  )
+  
+  # Egger's test (scalar V) 
+    egger_mod <- rma.mv(
+    yi     = data[[yi]],
+    V      = data[[vi]],
+    mods   = ~ 1 + sqrt_inv_e_n,
+    random = random_fx,
+    test   = "t",
+    method = "REML",
+    sparse = TRUE,
+    data   = data
+  )
+  
+  # Decline effect (VCV)
+  year_formula <- as.formula(paste0("~ 1 + ", year_var))
+  
+  year_mod <- rma.mv(
+    yi     = data[[yi]],
+    V      = if (!is.null(vcv)) vcv else data[[vi]],
+    mods   = year_formula,
+    random = random_fx,
+    test   = "t",
+    method = "REML",
+    sparse = TRUE,
+    data   = data
+  )
+  
+  # Multi-moderator bias model (VCV)
+  if (!is.null(bio_moderators)) {
+    multi_formula <- as.formula(
+      paste0("~ ", 
+             paste(bio_moderators, collapse = " + "),
+             " + sqrt_inv_e_n + ", year_var)
+    )
+    
+    multi_mod <- rma.mv(
+      yi     = data[[yi]],
+      V      = if (!is.null(vcv)) vcv else data[[vi]],
+      mods   = multi_formula,
+      random = random_fx,
+      test   = "t",
+      method = "REML",
+      sparse = TRUE,
+      data   = data
+    )
+  } else {
+    multi_mod <- NULL
+  }
+  
+  # Bubble plots
+  p_egger <- bubble_plot(egger_mod,
+                         mod   = "sqrt_inv_e_n",
+                         group = study_id,
+                         xlab  = "Square root of inverse effective sample size",
+                         ylab = "Effect size (lnRR)")
+  
+  p_year <- bubble_plot(year_mod,
+                        mod   = year_var,
+                        group = study_id,
+                        xlab  = "Publication year",
+                        ylab = "Effect size (lnRR)")
+  
+  if (!is.null(multi_mod)) {
+    p_multi_egger <- bubble_plot(multi_mod,
+                                 mod   = "sqrt_inv_e_n",
+                                 group = study_id,
+                                 xlab  = "Square root of inverse effective sample size",                                                         
+                                 ylab = "Effect size (lnRR)")
+    
+    p_multi_year <- bubble_plot(multi_mod,
+                                mod   = year_var,
+                                group = study_id,
+                                xlab  = "Publication year",
+                                ylab = "Effect size (lnRR)")
+  }
+  
+  # Return everything
+  list(
+    egger_model      = egger_mod,
+    year_model       = year_mod,
+    multi_model      = multi_mod,
+    plot_egger       = p_egger,
+    plot_year        = p_year,
+    plot_multi_egger = if (!is.null(multi_mod)) p_multi_egger else NULL,
+    plot_multi_year  = if (!is.null(multi_mod)) p_multi_year  else NULL
+  )
+}
+
+# Create quadratic term for intervention_dose
+clean_data$intervention_dose2 <- clean_data$intervention_dose^2
+
+results_all_data <- run_bias_models(
+  data           = clean_data,
+  vcv            = VCV,
+  year_var       = "publication_year",
+  bio_moderators = c("intervention_dose", 
+                     "intervention_dose2",
+                     "study_duration_days")
+)
+
+# Print results (all data)
+summary(results_all_data$egger_model)
+summary(results_all_data$year_model)
+summary(results_all_data$multi_model)
+
+# Print and save plots (all data)
+results_all_data$plot_egger 
+ggsave("plot_egger.png", plot = results_all_data$plot_egger, dpi = 300, width = 8, height = 6, units = "in")
+
+results_all_data$plot_year
+ggsave("plot_year.png", plot = results_all_data$plot_year, dpi = 300, width = 8, height = 6, units = "in")
+
+results_all_data$plot_multi_egger
+ggsave("plot_multi_egger.png", plot = results_all_data$plot_multi_egger, dpi = 300, width = 8, height = 6, units = "in")
+
+results_all_data$plot_multi_year
+ggsave("plot_multi_year.png", plot = results_all_data$plot_multi_year, dpi = 300, width = 8, height = 6, units = "in")
+
+# Print funnel plot
+funnel(res_3L_all, yaxis = "seinv", 
+      xlab = "Standarised residuals",
+      ylab = "Precision (inverse of SE)",
+      # xlim = c(-4.0, 4.5), 
+      # ylim = c(0.01, 60.0),
+      col = c(alpha("black", 0.5)),
+      cex = 1.0) 
+
+ggsave("funnel_all_data.png", dpi = 300, width = 8, height = 6, units = "in")
+
+## Testing if outcome category is a significant moderator
 res_outcome_fixed <- rma.mv(
   yi   = lnRR,
   V    = VCV,
@@ -214,102 +367,6 @@ res_outcome_fixed <- rma.mv(
   method = "REML"
 )
 summary(res_outcome_fixed)
-
-## Publication bias
-# Create function for assessing publication bias
-run_pub_bias <- function(data, outcome_name = NULL, random_structure = "~1 | study_ID/ES_ID",
-                         se_limit = NULL, x_limits = c(-2, 2), y_limit = NULL) {
-  
-  # Optional: subset by outcome category
-  if(!is.null(outcome_name)) {
-    data <- data %>% filter(outcome_category == outcome_name)
-  }
-  
-  # Compute SE, precision, effective sample size
-  data <- data %>%
-    mutate(
-      se_lnRR = sqrt(vi_lnRR),
-      precision = 1 / se_lnRR,
-      n_eff = (treatment_n * control_n) / (treatment_n + control_n),
-      inv_n_eff = 1 / n_eff
-    )
-  
-  # Fit 3-level meta-analysis with precision as moderator
-  res <- rma.mv(
-    yi = lnRR,
-    V  = vi_lnRR,
-    mods = ~ precision,
-    random = as.formula(random_structure),
-    data = data,
-    method = "REML"
-  )
-  
-  # Robust small-sample corrected test for the moderator
-  cr2_test <- coef_test(
-    res,
-    vcov = "CR2",
-    cluster = data$study_ID,
-    test = "Satterthwaite"
-  )
-  
-  # Funnel plot preparation
-  data <- data %>% mutate(SE = 1 / precision)
-  
-  precision_grid <- seq(min(data$precision, na.rm = TRUE),
-                        max(data$precision, na.rm = TRUE),
-                        length.out = 100)
-  
-  mean_lnRR <- mean(data$lnRR, na.rm = TRUE)
-  
-  bounds <- data.frame(
-    precision = precision_grid,
-    lnRR_upper = mean_lnRR + 1.96 / precision_grid,
-    lnRR_lower = mean_lnRR - 1.96 / precision_grid
-  )
-  
-  funnel_plot <- ggplot(data, aes(x = lnRR, y = precision)) +
-    geom_ribbon(
-      data = bounds,
-      aes(y = precision, xmin = lnRR_lower, xmax = lnRR_upper),
-      fill = "white",
-      colour = "black",
-      linetype = "dotted",
-      size = 0.5,
-      inherit.aes = FALSE
-    ) +
-    geom_point(shape = 21, fill = "grey", color = "black", size = 3) +
-    geom_vline(xintercept = 0, linetype = "dotted", color = "black", size = 1) +
-    scale_y_continuous(name = "Precision (1/SE)", limits = y_limit) +
-    scale_x_continuous(name = "Effect Size (lnRR)", limits = x_limits) +
-    theme_minimal(base_size = 14) +
-    theme(
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.x = element_blank(),
-      panel.grid.major.y = element_line(color = "white", size = 0.5),
-      panel.grid.minor.y = element_blank(),
-      axis.line = element_line(color = "black", linewidth = 0.8),
-      axis.ticks.length = unit(0.3, "cm"),
-      axis.ticks = element_line(color = "black"),
-      plot.title = element_text(face = "bold", hjust = 0.01, size = 18)
-    ) +
-    ggtitle(ifelse(is.null(outcome_name), "Funnel Plot", paste("Funnel Plot:", outcome_name)))
-  
-  # Return results
-  list(
-    model = res,
-    cr2_test = cr2_test,
-    funnel_plot = funnel_plot
-  )
-}
-
-# Run publication bias test on clean_data and print results
-pubbias_all <- run_pub_bias(clean_data)
-pubbias_all$model
-pubbias_all$cr2_test
-pubbias_all$funnel_plot
-
-# Save funnel plot
-ggsave("funnel_plot_all_data.png", width = 11, height = 8, units = "in") 
 
 ### Sensitivity analysis
 # Leave-one-out sensitivity analysis with VCV subsetting
@@ -348,69 +405,6 @@ sensitivity_results_VCV <- lapply(study_list, function(S) {
 }) %>% bind_rows()
 
 print(sensitivity_results_VCV) #Print sensitivity analysis results
-
-# Recreate funnel plot with S004 highlighted
-
-# Create plotting parameters outside of function
-clean_data <- clean_data %>%
-  mutate(
-    se_lnRR = sqrt(vi_lnRR),
-    precision = 1 / se_lnRR
-  )
-
-mean_lnRR <- mean(clean_data$lnRR, na.rm = TRUE)
-precision_grid <- seq(min(clean_data$precision), max(clean_data$precision), length.out = 100)
-bounds <- data.frame(
-  precision = precision_grid,
-  lnRR_upper = mean_lnRR + 1.96 / precision_grid,
-  lnRR_lower = mean_lnRR - 1.96 / precision_grid
-)
-
-# Funnel plot
-funnel_plot_S004 <- ggplot(clean_data, aes(x = lnRR, y = precision)) +
-  geom_ribbon(
-    data = bounds,
-    aes(y = precision, xmin = lnRR_lower, xmax = lnRR_upper),
-    fill = "white",
-    colour = "black",
-    linetype = "dotted",
-    linewidth = 0.5,
-    inherit.aes = FALSE
-  ) +
-
-  geom_point(aes(fill = ifelse(study_ID == "S004", "red", "grey")),
-             shape = 21,
-             color = "black",
-             size = 3) +
-
-  geom_vline(xintercept = 0, linetype = "dotted", color = "black", linewidth = 1) +
-
-  scale_fill_identity() +
-
-  scale_y_continuous(name = "Precision (1/SE)", limits = c(0, 400)) +
-  scale_x_continuous(name = "Effect Size (lnRR)", limits = c(-2.0, 2.0),
-                     breaks = seq(-5, 5, by = 0.5)) +
-
-  theme_minimal(base_size = 14) +
-  theme(
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.major.y = element_line(color = "white", linewidth = 0.5),
-    panel.grid.minor.y = element_blank(),
-    axis.line = element_line(color = "black", linewidth = 0.8),
-    axis.ticks.length = unit(0.3, "cm"),
-    axis.ticks = element_line(color = "black"),
-    axis.title = element_text(size = 18, face = "bold"),
-    axis.text = element_text(size = 16),
-    plot.title = element_text(face = "bold", hjust = 0.01, size = 18)
-  ) +
-  ggtitle("")
-
-# Print plot
-funnel_plot_S004
-
-# Save funnel plot
-ggsave("funnel_plot_S004_highlighted.png", width = 11, height = 8, units = "in") 
 
 # S004 is highly influential (> 100% pct_change) and flips the sign of lnRR. Removal improves publication bias risk.
 # Author (D. Francis) suggested that poor performance linked to poor diet stability
@@ -472,27 +466,94 @@ sens_data_plot <- run_orchard_plot(
 sens_data_plot   # Print orchard plot
 ggsave("sens_data_orchard_plot.png", width = 11, height = 8, units = "in")  # Save orchard plot
 
-# Re-test publication bias
-pubbias_sens <- run_pub_bias(clean_data_sens)
-pubbias_sens$model
-pubbias_sens$cr2_test
-pubbias_sens$funnel_plot
+# Publication bias with S004 removed
+results_sens_data <- run_bias_models(
+  data           = clean_data_sens,
+  vcv            = VCV_sens,
+  year_var       = "publication_year",
+  bio_moderators = c("intervention_dose", 
+                     "intervention_dose2",
+                     "study_duration_days")
+)
 
-# Removal of S004 reduces publication bias risk
+# Print results (S004 removed)
+summary(results_sens_data$egger_model)
+summary(results_sens_data$year_model)
+summary(results_sens_data$multi_model)
 
-# Save funnel plot
-ggsave("funnel_plot_sens_data.png", width = 11, height = 8, units = "in") 
+# Print and save plots (all data)
+results_sens_data$plot_egger 
+ggsave("plot_egger_sens.png", plot = results_sens_data$plot_egger_sens, dpi = 300, width = 8, height = 6, units = "in")
 
-## Test effect of outcome_category as moderator by running MLMR with OC as fixed effect 
+results_all_data$plot_year
+ggsave("plot_year_sens.png", plot = results_sens_data$plot_year_sens, dpi = 300, width = 8, height = 6, units = "in")
+
+results_all_data$plot_multi_egger
+ggsave("plot_multi_egger_sens.png", plot = results_sens_data$plot_multi_egger_sens, dpi = 300, width = 8, height = 6, units = "in")
+
+results_all_data$plot_multi_year
+ggsave("plot_multi_year_sens.png", plot = results_sens_data$plot_multi_year_sens, dpi = 300, width = 8, height = 6, units = "in")
+
+# Create funnel plot with S004 excluded
+funnel(res_3L_sens, yaxis = "seinv", 
+      xlab = "Standarised residuals",
+      ylab = "Precision (inverse of SE)",
+      # xlim = c(-4.0, 4.5), 
+      # ylim = c(0.01, 60.0),
+      col = c(alpha("black", 0.5)),
+      cex = 1.0) 
+ggsave("funnel_sens.png", dpi = 300, width = 8, height = 6, units = "in")
+
+## Test effect of outcome_category as moderator by running MLMR with OC as fixed effect (for both full dataset and with S004 removed)
 # Run MLMR (no intercept model)
-res_meta_reg <- rma.mv(yi = lnRR, V  = VCV_sens,
+
+# Full dataset
+res_meta_reg <- rma.mv(yi = lnRR, V  = VCV,
+  mods = ~ 0 + outcome_category,        
+  random = list(                
+    ~ 1 | study_ID / ES_ID),
+  data = clean_data,
+  method = "REML"
+)
+res_meta_reg
+
+# Extract variance components for res_meta_reg_sens
+I2_sens_all_data_mlmr <- calc_I2_3level(res_meta_reg)
+print(I2_sens_all_data_mlmr)
+
+# Create orchard plot for sensitivity data
+all_data_mlmr_plot <- run_orchard_plot(
+  model = res_meta_reg,
+  I2 = I2_sens_all_data_mlmr,
+  title = ""
+)
+
+all_data_mlmr_plot
+ggsave("all_data_mlmr_plot.png", dpi = 300, width = 8, height = 6, units = "in")
+
+# S004 removed
+res_meta_reg_sens <- rma.mv(yi = lnRR, V  = VCV_sens,
   mods = ~ 0 + outcome_category,        
   random = list(                
     ~ 1 | study_ID / ES_ID),
   data = clean_data_sens,
   method = "REML"
 )
-res_meta_reg
+res_meta_reg_sens
+
+# Extract variance components for res_meta_reg_sens
+I2_sens_mlmr <- calc_I2_3level(res_meta_reg_sens)
+print(I2_sens_mlmr)
+
+# Create orchard plot for sensitivity data
+sens_mlmr_plot <- run_orchard_plot(
+  model = res_meta_reg_sens,
+  I2 = I2_sens_mlmr,
+  title = ""
+)
+
+sens_mlmr_plot
+ggsave("sens_mlmr_plot.png", plot = sens_mlmr_plot, dpi = 300, width = 8, height = 6, units = "in")
 
 ### Outcome category is a significant moderator
 ## Create sub-groups for each outcome category and run MLMA
@@ -569,41 +630,6 @@ names(I2_results_subgroups) <- outcome_list
 
 # Print results
 I2_results_subgroups
-
-# Create orchard_plots for all subgroup MLMA
-orchard_plots_subgroup <- lapply(names(results_mlma), function(cat) {
-  
-  res <- results_mlma[[cat]]
-  if(is.null(res)) return(NULL)
-  
-  I2 <- calc_I2_3level(res$model)
-  
-  run_orchard_plot(
-    model = res$model,
-    I2 = I2,
-    title = paste(cat)
-  )
-})
-
-# Assign letters for subplots and new titles
-names(orchard_plots_subgroup) <- names(results_mlma)
-orchard_plots_clean <- orchard_plots_subgroup[sapply(orchard_plots_subgroup, function(p) inherits(p, "gg"))]
-letters <- paste0(LETTERS[1:length(orchard_plots_clean)], ")")
-new_titles <- paste(letters, names(orchard_plots_subgroup))
-
-orchard_plots_titled <- mapply(function(plot, title_new) {
-  plot +
-    ggtitle(title_new) +
-    theme(plot.title = element_text(size = 14)) +
-    scale_fill_manual(values = "grey") +
-    scale_colour_manual(values = "grey")
-}, orchard_plots_clean, new_titles, SIMPLIFY = FALSE)
-
-# Create stacked plot with all outcome categories
-multi_panel_orchard <- wrap_plots(orchard_plots_titled, nrow = 1)
-
-multi_panel_orchard   # Print orchard plot
-ggsave("multi_panel_orchard.png", width = 15, height = 6, units = "in")  # Save orchard plot
 
 ## Assess correlation between continious moderators before choosing variables for inclusion in MLMA 
 # Correlation between continuous moderators (code has been adapted from Ayumi et al. (https://zenodo.org/records/13147019))
@@ -777,10 +803,5 @@ ulva_dose <- bubble_plot(res_meta_dose,
 # Print plot
 ulva_dose
 
-# Save funnel plot
+# Save ulva_dose_pot
 ggsave("ulva_dose_relationship.png", width = 8, height = 6, units = "in") 
-
-
-# Save plot
-ggsave("ulva_dose_relationship.png", width = 8, height = 6, units = "in") 
-
